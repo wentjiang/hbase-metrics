f1218550963cae44465752cfb553a317ac384d5a
wentjiang <wentjiang@sina.com>
2022-11-21 23:33:19 +0800

    format code

--- hbase-metrics-agent/src/main/java/com/wentjiang/agent/HBaseByteBuddyAgent.java
+++ hbase-metrics-agent/src/main/java/com/wentjiang/agent/HBaseByteBuddyAgent.java
@@ -1,6 +1,5 @@
 package com.wentjiang.agent;
 
-import com.wentjiang.agent.InstanceMethodsAroundInterceptor;
 import com.wentjiang.agent.common.InstMethodsInter;
 import com.wentjiang.agent.impl.HRegionDoWALAppendInterceptor;
 import com.wentjiang.agent.impl.MockReadHBaseCacheInterceptor;
@@ -10,39 +9,22 @@ import net.bytebuddy.matcher.ElementMatchers;
 
 import java.lang.instrument.Instrumentation;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static com.wentjiang.agent.util.AgentUtils.transferMethodInfo2ElementMatcher;
-import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class HBaseByteBuddyAgent {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-
-        // System.out.println("premain in HBaseByteBuddyAgent");
-
-        new AgentBuilder.Default().type(ElementMatchers.nameContainsIgnoreCase("HMaster"))
-                .transform((builder, typeDescription, classLoader, module, protectionDomain) -> {
-                    System.out.println("start transform!");
-                    return builder.method(named("run"))
-                            .intercept(MethodDelegation.withDefaultConfiguration().to(new InstMethodsInter(
-                                    "com.wentjiang.agent.impl.MockReadHBaseCacheInterceptor", classLoader)));
-                }).with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
-                // .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-                .installOn(inst);
-
-        // for (InstanceMethodsAroundInterceptor interceptor : getInterceptorList()) {
-        // enhanceClass(interceptor, inst);
-        // }
-
-        // System.out.println("premain in HBaseByteBuddyAgent done");
+        System.out.println("premain in HBaseByteBuddyAgent");
+        for (InstanceMethodsAroundInterceptor interceptor : getInterceptorList()) {
+            enhanceClass(interceptor, inst);
+        }
+        System.out.println("premain in HBaseByteBuddyAgent done");
     }
 
     public static List<InstanceMethodsAroundInterceptor> getInterceptorList() {
-        return Arrays.asList(
-                // new HRegionDoWALAppendInterceptor(),
-                new MockReadHBaseCacheInterceptor());
+        return Arrays.asList(new HRegionDoWALAppendInterceptor(), new MockReadHBaseCacheInterceptor());
     }
 
     public static void enhanceClass(InstanceMethodsAroundInterceptor interceptor, Instrumentation inst) {
